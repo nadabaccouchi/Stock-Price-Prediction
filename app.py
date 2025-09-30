c2977dbc2b1bd42838ec9e9dd7811d96da570034
nadabaccouchi <nadabaccouchi176@gmail.com>
2025-09-30 22:19:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model(r'C:\Users\nada\Desktop\stock market price pred\Stock Predictions Model.keras')
+model = model = load_model("Stock Predictions Model.keras")
 
 st.header('Stock Market Predictor')
 
@@ -77,4 +77,5 @@ plt.plot(y, 'g', label = 'Predicted Price')
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.show()
+
 st.pyplot(fig4)
